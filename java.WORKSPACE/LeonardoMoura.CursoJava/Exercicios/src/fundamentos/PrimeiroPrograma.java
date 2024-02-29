e1054278ee9ce6857d7a48ee86fd9f39130be85c
LutekDev <lutekdev@gmail.com>
2024-02-28 21:48:17 -0300

    📦 NEW: Added New Files in 28/02/24

--- java.WORKSPACE/LeonardoMoura.CursoJava/Exercicios/src/fundamentos/PrimeiroPrograma.java
+++ java.WORKSPACE/LeonardoMoura.CursoJava/Exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -3,5 +3,6 @@ package fundamentos;
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Hello, World From Java!");
+		System.out.println("My First Program in Java!");
 	}
 }
